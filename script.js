09e21cb279329820868bf578df54d0de37e1fb8f
Matias Baccega <mbaccega29@gmail.com>
2022-04-17 22:14:53 -0300

    Ciclo

---
+++ script.js
@@ -0,0 +1,12 @@
+let num = 10;
+
+do {
+    num = parseInt(prompt("Bienvenido al desafio complementario. Ingrese un numero del 1 al 10"));
+
+    if(num <= 10){
+        alert ("El numero ingresado es =" + " " + num)
+    }
+    else
+        alert("Ingrese un numero del 1 al 10 por favor!")
+}
+while (num > 10);
